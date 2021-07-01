3cd216050358fa33f8764130e4a072415beb3f4a
Tanvir Rahman Khan <aeontanvir@gmail.com>
2021-07-01 16:44:34 +0600

    Cleaning and create necessary component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a class="active item"> Home </a>
+    <a class="item"> Messages </a>
+    <a class="item"> Friends </a>
+    <div class="right menu">
+      <div class="item">
+        <div class="ui icon input">
+          <input type="text" placeholder="Search..." />
+          <i class="search link icon"></i>
+        </div>
+      </div>
+      <a class="ui item"> Log In </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style>
+</style>
